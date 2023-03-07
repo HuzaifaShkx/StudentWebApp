8e2b104f1ccae8b8a5d04f81456090b272b84acd
HuzaifaShkx <80284490+HuzaifaShkx@users.noreply.github.com>
2023-03-07 08:39:34 +0500

    Add project files.

---
+++ StudentWebApp/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentWebApp.Models;
+
+namespace StudentWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult StudentEntry()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ StudentWebApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentWebApp.Models
+{
+    public class Student
+    {
+        public int rollno { get; set; }
+        public string name { get; set; }
+        public string program { get; set; }
+        public char gender { get; set; }
+        public bool isjob { get; set; }
+    }
+}
