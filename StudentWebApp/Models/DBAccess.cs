458b6e6d3a7dbb74c9b94893c59bcbe825c24c22
HuzaifaShkx <80284490+HuzaifaShkx@users.noreply.github.com>
2023-03-07 14:23:39 +0500

    Added data base connectivity and InsertUpdateDelete method

--- StudentWebApp/Models/DBAccess.cs
+++ StudentWebApp/Models/DBAccess.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace StudentWebApp.Models
 {
     public class DBAccess
     {
         //contains the information of server and data(connection information)
-        static string constr = " ";
+        static string constr = @"Data Source=DESKTOP-H5AAPPB; initial catalog=StudentDB;integrated security=true";//connection string
+        SqlConnection con = new SqlConnection(constr);
+        SqlCommand cmd = null;
+        public void openConnection() {
+            if (con.State == ConnectionState.Closed) {
+                con.Open(); 
+            }
+        }
+
+        public void closeConnection()
+        {
+            if(con.State == ConnectionState.Open) {
+                con.Close();
+            }
+        }
+
+        public void InsertUpdateDelete(string query)
+        {
+            cmd=new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+
+        }
     }
 }
