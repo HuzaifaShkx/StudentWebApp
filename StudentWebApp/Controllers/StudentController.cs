f89ae6dea6dfd0c03f892ea6cbd45812450878a9
HuzaifaShkx <80284490+HuzaifaShkx@users.noreply.github.com>
2023-03-07 15:01:01 +0500

    Added data in database table

--- StudentWebApp/Controllers/StudentController.cs
+++ StudentWebApp/Controllers/StudentController.cs
@@ -20,11 +20,17 @@ namespace StudentWebApp.Controllers
         [HttpPost]
         public ActionResult StudentEntry(Student s)
         {
+            
             string name = s.name;
             int rollno=s.rollno;
             string prog = s.program;
             bool job = s.isjob;
             char gender = s.gender;
+            string q = "insert into Student(Rollno,Name,Program,Gender,isJob) values('"+s.rollno+"','"+s.name+"','"+s.program+"','"+s.gender+"','"+s.isjob+"')";
+            DBAccess obj=new DBAccess();
+            obj.openConnection();
+            obj.InsertUpdateDelete(q);
+            obj.closeConnection();
             return View();
         }
     }
